70b28f069fb11f2f9a0f937a4f4a6118c4c08051
alz2019 <az2019git@rambler.ru>
2020-10-03 10:17:55 +0300

    Comments to RegionDao

--- src/main/java/com/alz2019/tracker/data/RegionDao.java
+++ src/main/java/com/alz2019/tracker/data/RegionDao.java
@@ -6,7 +6,15 @@ import java.io.IOException;
 import java.util.List;
 
 public interface RegionDao {
+    /**
+     * Fetches data from JSON. Updates regional stats
+     */
     void fetchData();
 
+    /**
+     * Retrieves and returns all stats
+     *
+     * @return list of regions with stats
+     */
     List<Region> getAllStats();
 }
