f9405b86e35ba213cf5ea9aa5d0fc7e1a9c8d038
alz2019 <az2019git@rambler.ru>
2020-10-22 08:46:25 +0300

    Two tests added

--- src/test/java/com/alz2019/tracker/TrackerApplicationTests.java
+++ src/test/java/com/alz2019/tracker/TrackerApplicationTests.java
@@ -1,13 +1,31 @@
 package com.alz2019.tracker;
 
+import com.alz2019.tracker.dao.RegionDao;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TrackerApplication.class)
 class TrackerApplicationTests {
+    @Autowired
+    RegionDao dao;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void testListSize() {
+        assertNotEquals(0, dao.getAllStats().size());
+    }
+
+    @Test
+    void testListNotContains() {
+        assertTrue(dao.getAllStats().stream().noneMatch(region -> region.getName().contains("Минск")));
+    }
 }
